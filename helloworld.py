6d7c8bf6b9454d6d1fae66a951e4995968e69960
kristofkatona <kristof.katona@wallrust.com>
2016-06-26 20:03:33 +0200

    added practice file helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import time
+import os.path
+
+lastmodified = os.path.getmtime("helloWorld.py")
+elapsed = time.time() - lastmodified
+# print (elapsed)
+if elapsed > 5 :
+	print("OLD FILE DID YOU SAVE %s") % time.ctime(lastmodified)
+ 
