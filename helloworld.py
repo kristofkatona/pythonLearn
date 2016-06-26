edd12a469f6b719b8bd6283b5d8e87d03b2be6a0
kristofkatona <kristof.katona@wallrust.com>
2016-06-26 20:35:51 +0200

    sqrt newton

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,22 @@
 import time
 import os.path
 
-def warnlongtimenosave() ;
+def warnlongtimenosave() :
 	lastmodified = os.path.getmtime("helloWorld.py")
 	elapsed = time.time() - lastmodified
 	# print (elapsed)
 	if elapsed > 5 :
 		print("OLD FILE DID YOU SAVE %s") % time.ctime(lastmodified)
  
- warnlongtimenosave()
- 
+warnlongtimenosave()
+
+def sqrt(a, n) :
+	xn = 1.0
+	i = 1
+	while i <= n :
+		xn = (xn + a / xn ) / 2
+		print(xn)
+		i = i + 1
+	return xn
+
+print(sqrt(5,100))
